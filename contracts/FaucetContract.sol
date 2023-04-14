b5fb07c397c3adcdacd5c73b95d4ce23a2f82d00
Raul Ceron <receronp@gmail.com>
2023-04-14 15:46:54 -0600

    Adds funders(addr->bool) LUT for deduplication.

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,23 +3,29 @@ pragma solidity >=0.8.0 <0.9.0;
 
 contract Faucet {
     uint public numOfFunders;
-    mapping(uint => address) public funders;
+    mapping(address => bool) public funders;
+    mapping(uint => address) public lutFunders;
 
     receive() external payable {}
 
     function addFunds() external payable {
-        funders[numOfFunders++] = msg.sender;
+        address funder = msg.sender;
+
+        if (!funders[funder]) {
+            funders[funder] = true;
+            lutFunders[numOfFunders++] = funder;
+        }
     }
 
     function getAllFunders() external view returns (address[] memory) {
         address[] memory _funders = new address[](numOfFunders);
         for (uint256 ii = 0; ii < numOfFunders; ii++) {
-            _funders[ii] = funders[ii];
+            _funders[ii] = lutFunders[ii];
         }
         return _funders;
     }
 
     function getFunderAtIndex(uint8 index) external view returns (address) {
-        return funders[index];
+        return lutFunders[index];
     }
 }
