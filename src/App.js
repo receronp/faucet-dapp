790b173f31b3b5e9deaa2bf5d939901fb4f358d2
Raul Ceron <receronp@gmail.com>
2023-04-15 02:18:46 -0600

    Adds bulma and css style files.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import "./App.css";
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>Webpack App</h1>
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="balance-view is-size-2">
+            Current Balance: <strong>10</strong> ETH
+          </div>
+          <button className="button is-link mr-2">Donate</button>
+          <button className="button is-primary">Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 };
 
