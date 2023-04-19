7175a615acd8ae377a5868fe738264072eb123ba
Raul Ceron <receronp@gmail.com>
2023-04-19 04:09:09 -0600

    Adds web3 dependencies, updates front.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Web3 from "web3";
 
 const App = () => {
+  const [web3API, setWeb3API] = useState({ web3: null, provider: null });
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      let provider = null;
+      if (window && window.ethereum) {
+        // We are in the browser and metamask is running.
+        provider = window.ethereum;
+        setWeb3API({ web3: new Web3(provider), provider });
+      }
+    };
+
+    loadProvider();
+  }, []);
+
+  useEffect(() => {
+    const getAccounts = async () => {
+      const accounts = await web3API.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+    web3API.web3 && getAccounts();
+  }, [web3API.web3]);
+
   return (
     <>
       <div className="faucet-wrapper">
         <div className="faucet">
-          <div className="balance-view is-size-2">
+          <div className="is-flex is-align-items-center">
+            <span>
+              <strong className="mr-2">Account: </strong>
+            </span>
+            {account ? (
+              account
+            ) : (
+              <button
+                className="button is-small"
+                onClick={async () => {
+                  try {
+                    await web3API.provider.request({
+                      method: "eth_requestAccounts",
+                    });
+                    location.reload();
+                  } catch (e) {
+                    console.log(e);
+                  }
+                }}
+              >
+                Connect Wallet
+              </button>
+            )}
+          </div>
+          <div className="balance-view is-size-2 my-4">
             Current Balance: <strong>10</strong> ETH
           </div>
           <button className="button is-link mr-2">Donate</button>
